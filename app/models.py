ff573fd228fb3e49824dc4fb583098be8345d797
shlaikov <shlaikov.alexey@ya.ru>
2017-06-06 02:53:42 +0300

    Added some code.

--- app/models.py
+++ app/models.py
@@ -5,13 +5,13 @@ from random import choice
 import string
 
 
-class Greeting(models.Model):
-    when = models.DateTimeField('date created', auto_now_add=True)
-
-
 def generate_key():
     chars = string.digits + string.ascii_letters
     return ''.join(choice(chars) for _ in range(3))
+
+
+class Greeting(models.Model):
+    when = models.DateTimeField('date created', auto_now_add=True)
 
 
 class ShortUrl(models.Model):
@@ -19,14 +19,13 @@ class ShortUrl(models.Model):
     target = models.URLField(max_length=255, null=False, blank=True, unique=True)
     added = models.DateTimeField(auto_now_add=True, editable=False)
 
-
-def __unicode__(self):
-    return '%s %s' % (self.target, self.key)
+    def __unicode__(self):
+        return '%s %s' % (self.target, self.key)
 
 
 class Hit(models.Model):
     target = models.ForeignKey(ShortUrl)
     time = models.DateTimeField(auto_now_add=True, editable=False)
-    referer = models.URLField(blank=True)
-    ip = models.GenericIPAddressField(blank=True, null=True)
+    referer = models.URLField(blank=True, editable=False)
+    ip = models.GenericIPAddressField(blank=False)
     user_agent = models.CharField(blank=True, max_length=100)
